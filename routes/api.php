235bf2feff686a0846aadc4885d326b1051d4a25
Raul Marcolino <luiz.junior53@aluno.ce.gov.br>
2025-03-27 08:51:25 -0300

    commit

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,22 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
-    return response()->json([ 'message' => 'API is working' ]);
+    return response()->json([
+        'message' => 'API is working'
+    ]);
+});
+
+Route::post('/register', function(Request $request) {
+    $user = new User();
+    $user->name = $request->name;
+    $user->email = $request->email;
+    $user->password = $request->password;
+    $user->save();
+    return response()->json([
+        "user" => $user
+    ]);
 });
